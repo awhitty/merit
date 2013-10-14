6e845b0dc4519af4a9c3ec451c86711766305936
Austin Whittier <austin.whitt@gmail.com>
2013-10-14 16:53:02 -0700

    Trying fixes for static files

--- app/main/templatetags/admin_static.py
+++ app/main/templatetags/admin_static.py
@@ -3,6 +3,9 @@ from django.template import Library
 
 register = Library()
 
-from django.contrib.staticfiles.templatetags.staticfiles import static
+if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+	from django.contrib.staticfiles.templatetags.staticfiles import static
+else:
+	from django.templatetags.static import static
 
 static = register.simple_tag(static)
